1fddfb6b3ce3ff0d16db8371a427df4296aa5d14
Guilherme Amaral <guilamar@student.42sp.org.br>
2026-03-17 21:11:45 -0300

    Level05

--- testes/Level05/biggest_paw/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-"$1" "abcba"
-"$1" "abacabaca"
-"$1" "a"
-"$1" "xabacabacaxy"
-"$1"
--- testes/Level05/print_vet_meds/main.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-void print_vet_meds(const void *addr, size_t size);
-int main(void) {
-    // Array com bytes exatos para evitar problemas de endianness no diff
-    unsigned char tab[16] = {0, 1, 2, 3, 255, 65, 66, 67, 10, 9, 32, 126, 127, 48, 49, 50};
-    print_vet_meds(tab, 16);
-    return 0;
-}
